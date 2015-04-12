f51a0440a1a8da28a385ccf793eb682326203542
Jiayin Tang <jt2823@columbia.edu>
2015-04-12 08:35:06 -0400

    v1
    web scraper for snapple facts

---
+++ README.txt
@@ -0,0 +1,5 @@
+Simple web scraper (done during HackPrinceton)
+
+used virtualenv with requests and beautiful soup (such beauty)
+
+branches: played around with twitter?
---
+++ python-scraper.py
@@ -0,0 +1,24 @@
+# run with virtualenv
+# install requests beautifulsoup
+
+'''produces a large txt file of all the snapple facts'''
+
+import requests
+import bs4
+
+base_url = 'http://www.snapple.com/real-facts/list-view/'
+
+def get_snapple_fact():
+	outfile = open('facts.txt','w')
+
+	for i in range(1,11):
+		url = base_url + str(i)
+		response = requests.get(url)
+		soup = bs4.BeautifulSoup(response.text)
+		d = soup.find_all('p', class_="fact_detail")
+		for element in d:
+			element = str(element).strip('</p>')
+			element = element.strip('<p class="fact_detail">')
+			outfile.write(element + '\n')
+
+get_snapple_fact() 
